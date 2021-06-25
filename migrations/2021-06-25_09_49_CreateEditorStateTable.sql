0a8d58d5da46bea8dee9b1fb59cbf5da3c178b2e
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2021-06-25 14:51:05 +0200

    Update 2021-06-25_09_49_CreateEditorStateTable.sql

--- migrations/2021-06-25_09_49_CreateEditorStateTable.sql
+++ migrations/2021-06-25_09_49_CreateEditorStateTable.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `spinned_content_video_editor_state` (
   `id` int NOT NULL,
   `video_id` int NOT NULL,
-  `state` text NOT NULL,
-  `remotion_state` text NOT NULL,
+  `clips` text NOT NULL,
+  `texts` text NOT NULL,
+  `clip_maker_props` text NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
