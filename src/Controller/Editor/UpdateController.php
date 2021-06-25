ef55322c7fbab1c77abd963f4cf990a676db418c
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2021-06-25 15:10:00 +0200

    Update UpdateController.php

--- src/Controller/Editor/UpdateController.php
+++ src/Controller/Editor/UpdateController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller\Editor;
 
+use App\Command\Editor\UpdateCommand;
 use App\Http\Request\JsonBodyParser;
 
 class UpdateController
 {
     public function __construct(
-        private JsonBodyParser $parser
+        private JsonBodyParser $parser,
+        private UpdateCommand $command
     )
     {
     }
 
-    public function __invoke(int $contentId, ?string $body): void
+    public function __invoke(int $videoId, ?string $body): void
     {
         $jsonBody = $this->parser->parse($body);
 
@@ -24,15 +26,14 @@ class UpdateController
             http_response_code(400);
             return;
         }
-
-        var_dump($jsonBody); die;
-/*
-        $id = $this->command->execute(
-            $contentId,
-            $jsonBody['name'],
-            $jsonBody['width'],
-            $jsonBody['height']
-        );*/
+        
+        $this->command->execute(
+            $videoId,
+            $jsonBody['clips'],
+            $jsonBody['texts'],
+            $jsonBody['clipMakerProps']
+        );
+        
         http_response_code(204);
     }
 }
