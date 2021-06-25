b2575e009e466a58fb174798ce4220a2852a0d35
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-06-25 23:38:57 +0200

    oups

--- src/Normalizer/NormalizerFactory.php
+++ src/Normalizer/NormalizerFactory.php
@@ -10,7 +10,7 @@ class NormalizerFactory
         $videoNormalizer = new VideoNormalizer(new Normalizer([$defaultNormalizer]));
 
         return new Normalizer([
-            new VideoDetailNormalizer(new Normalizer([$videoNormalizer])),
+            new VideoDetailNormalizer(new Normalizer([$videoNormalizer, $defaultNormalizer])),
             $videoNormalizer,
             $defaultNormalizer
         ]);
