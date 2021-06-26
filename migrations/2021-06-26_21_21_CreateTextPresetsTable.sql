44c322681112165dedca52c716ac44bca0ac20e2
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-06-26 21:26:03 +0200

    text presets table

---
+++ migrations/2021-06-26_21_21_CreateTextPresetsTable.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `channel_storage`.`spinned_content_text_preset` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `content` TEXT NOT NULL , `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;
