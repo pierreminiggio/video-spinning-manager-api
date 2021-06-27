1f4256053fedef4aeff50cc0da3c98f94c4e5c50
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-06-27 12:21:05 +0200

    render status table

---
+++ migrations/2021-06-27_12_06_CreateRenderStatusesTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `channel_storage`.`spinned_content_video_render_status` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `video_id` INT NOT NULL,
+    `finished_at` DATETIME NULL,
+    `file_path` VARCHAR(255) NULL,
+    `failed_at` DATETIME NULL,
+    `failed_reason` TEXT NULL,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
