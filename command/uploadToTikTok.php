eaa2fee00bb33bf6457ab9eb08c82fb5560e8c11
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-07-01 20:31:58 +0200

    get ids

---
+++ command/uploadToTikTok.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Query\Video\TikTokVideosToUploadQuery;
+use PierreMiniggio\ConfigProvider\ConfigProvider;
+use PierreMiniggio\DatabaseConnection\DatabaseConnection;
+use PierreMiniggio\DatabaseFetcher\DatabaseFetcher;
+
+require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$configProvider = new ConfigProvider(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
+$config = $configProvider->get();
+
+$dbConfig = $config['db'];
+$fetcher = new DatabaseFetcher(new DatabaseConnection(
+    $dbConfig['host'],
+    $dbConfig['database'],
+    $dbConfig['username'],
+    $dbConfig['password'],
+    DatabaseConnection::UTF8_MB4
+));
+
+$query = new TikTokVideosToUploadQuery($fetcher);
+$tikTokIdsToUpload = $query->execute();
+
+foreach ($tikTokIdsToUpload as $tikTokIdToUpload) {
+    // todo recup tiktok infos
+    // todo recup upload status infos
+    // todo recup render status infos
+}
---
+++ src/Query/Video/TikTokVideosToUploadQuery.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Query\Video;
+
+use App\Enum\UploadTypeEnum;
+use PierreMiniggio\DatabaseFetcher\DatabaseFetcher;
+
+class TikTokVideosToUploadQuery
+{
+    public function __construct(
+        private DatabaseFetcher $fetcher
+    )
+    {
+    }
+
+    /**
+     * @return int[]
+     */
+    public function execute(): array
+    {
+        $orderedUploadStatusQuery = $this->fetcher->createQuery(
+            $this->fetcher->createQuery(
+                'spinned_content_upload_status'
+            )->select(
+                'upload_id',
+                'max(id) as last_id'
+            )->groupBy(
+                'upload_id'
+            )->where(
+                'upload_type = "' . UploadTypeEnum::TIKTOK . '"'
+            ),
+            'lus'
+        )->select(
+            'lus.last_id as id',
+            'lus.upload_id',
+            'us.finished_at',
+            'us.failed_at'
+        )->join(
+            'spinned_content_upload_status as us',
+            'us.id = lus.last_id'
+        )->where(
+            'us.upload_type = "' . UploadTypeEnum::TIKTOK . '"'
+        );
+
+        $queriedVideos = $this->fetcher->query(
+            $this->fetcher->createQuery(
+                'spinned_content_video',
+                'v'
+            )->join(
+                'spinned_content_tiktok_upload as tu',
+                'tu.video_id = v.id'
+            )->join(
+                '(' . $orderedUploadStatusQuery->build() . ') as cus',
+                'cus.upload_id = tu.id'
+            )->select(
+                'tu.id'
+            )->where(
+                'v.finished_at IS NOT NULL
+                AND (
+                    (cus.finished_at IS NULL AND cus.failed_at IS NOT NULL)
+                    OR cus.id IS NULL
+                )
+                AND tu.id IS NOT NULL
+                AND tu.publish_at <= NOW()'
+            )->orderBy(
+                'tu.publish_at'
+            )
+        );
+
+        return array_map(fn (array $queriedVideo) => (int) $queriedVideo['id'], $queriedVideos);
+    }
+}
