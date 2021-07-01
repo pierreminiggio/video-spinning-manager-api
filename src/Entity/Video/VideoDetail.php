f06dfd4ff0790063ec4965bb258fbaef7e2bc0f9
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2021-07-01 11:43:39 +0200

    Update VideoDetail.php

--- src/Entity/Video/VideoDetail.php
+++ src/Entity/Video/VideoDetail.php
@@ -6,12 +6,16 @@ use App\Entity\Account\AccountCollection;
 
 class VideoDetail
 {
+    /**
+     * @param int[] $postedOnAccounts
+     */
     public function __construct(
         public Video $video,
         public bool $downloaded,
         public bool $hasRenderedPreview,
         public EditorState $editorState,
-        public AccountCollection $spinnedAccountSocialMediasAccounts
+        public AccountCollection $spinnedAccountSocialMediasAccounts,
+        public array $postedOnAccounts
     )
     {
     }
