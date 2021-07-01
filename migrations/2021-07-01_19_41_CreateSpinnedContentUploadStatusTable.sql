45f3253fe088d542264d6f77ffe2f4c7e0d9ed2d
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-07-01 19:42:43 +0200

    upload status table

---
+++ migrations/2021-07-01_19_41_CreateSpinnedContentUploadStatusTable.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `spinned_content_upload_status` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `upload_type` VARCHAR(30) NOT NULL,
+    `upload_id` INT NOT NULL,
+    `finished_at` DATETIME NULL,
+    `remote_url` TEXT NULL,
+    `remote_id` VARCHAR(255) NULL,
+    `failed_at` DATETIME NULL,
+    `fail_reason` DATETIME NULL,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
