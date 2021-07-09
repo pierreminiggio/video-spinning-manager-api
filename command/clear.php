873ab3a8c0e429c89960707786ee540e94ec1ae1
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-07-09 23:12:28 +0200

    clear

--- command/clear.php
+++ command/clear.php
@@ -5,7 +5,9 @@ use PierreMiniggio\ConfigProvider\ConfigProvider;
 use PierreMiniggio\DatabaseConnection\DatabaseConnection;
 use PierreMiniggio\DatabaseFetcher\DatabaseFetcher;
 
-require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+$projectDir = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
+
+require $projectDir . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 $configProvider = new ConfigProvider(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
 $config = $configProvider->get();
@@ -19,7 +21,46 @@ $fetcher = new DatabaseFetcher(new DatabaseConnection(
     DatabaseConnection::UTF8_MB4
 ));
 
+$cacheDir = $projectDir . 'cache' . DIRECTORY_SEPARATOR;
+
 $doneVideoQuery = new DoneVideoQuery($fetcher);
 
-$doneVideoIds = $doneVideoQuery->execute();
-var_dump($doneVideoIds);
+$doneVideos = $doneVideoQuery->execute();
+
+$cacheContent = scandir($cacheDir);
+
+/** @var int[] $clearedContents */
+$clearedContents = [];
+
+foreach ($doneVideos as $doneVideo) {
+
+    $contentId = $doneVideo->contentId;
+
+    if (in_array($contentId, $clearedContents)) {
+        continue;
+    }
+
+    $videoPath = $cacheDir . $contentId . '.mp4';
+
+    if (file_exists($videoPath)) {
+        unlink($videoPath);
+    }
+
+    $thumbnails = array_filter(
+        $cacheContent,
+        fn (string $thumbnail) => str_starts_with(
+            $thumbnail,
+            $contentId . '-'
+        ) && str_ends_with(
+            $thumbnail,
+            '.png'
+        )
+    );
+
+    foreach ($thumbnails as $thumbnail) {
+        $thumbnailPath = $cacheDir . $thumbnail;
+        unlink($thumbnailPath);
+    }
+
+    $clearedContents[] = $contentId;
+}
